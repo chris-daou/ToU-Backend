ad6a5d78b8f981c796e89f1919c77e2d07500e2b
chris-daou <117108446+chris-daou@users.noreply.github.com>
2023-04-18 12:27:08 +0300

    added more dependencies

--- controllers/TravelerController.js
+++ controllers/TravelerController.js
@@ -11,3 +11,22 @@ require('dotenv').config();
 const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
+
+aws.config.update({
+    secretAccessKey: process.env.ACCESS_SECRET,
+    accessKeyId: process.env.ACCESS_KEY,
+    region: process.env.REGION
+})
+const BUCKET = process.env.BUCKET
+const s3 = new aws.S3();
+
+let transporter = nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'donotreply.tou.lebanon@outlook.com', // your email address
+      pass: '*31&pCbE' // your email password
+    }
+  });
+
