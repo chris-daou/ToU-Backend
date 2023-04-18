a302e847a20679fcfa93724c2f52779aacc0cb6d
vickcodes <vicken.k8@gmail.com>
2023-04-18 12:42:19 +0300

    Added Admin API to get all active Travelers

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -250,3 +250,20 @@ module.exports.get_waitingpending_get = async (req, res) => {
     res.send(orders);
 }
 
+
+
+module.exports.getActiveTravelers_get = async (req, res) => {
+    try{
+        const activeTravelers = await Traveler.find({active: true, provided_pickup: false, revoked: false});
+        if(activeTravelers.length>0){
+            res.status(200).json(activeTravelers);
+        }else{
+            res.status(404).json({message: 'No active travelers were found.'})
+        }
+    }catch(err){
+        console.log(err);
+        res.status(500).json({ message: 'Server Error Occured'})
+    }
+    
+}
+
