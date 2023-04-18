fc3cb8c884bc2b05a02dae59431d17ba6beefa80
vickcodes <vicken.k8@gmail.com>
2023-04-18 12:52:41 +0300

    Added Admin API to get

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -444,3 +444,19 @@ module.exports.deleteTraveler = async (req, res) => {
         console.log(err);
     }
 }
+
+
+module.exports.getrevokedTraveler_get = async(req, res) => {
+    const travelerId = req.params.travelerid;
+    const traveler = await Traveler.findById(travelerId);
+    try{
+        if(traveler){
+            res.status(200).json(traveler);
+        }else{
+            res.status(404).json( {message: 'Traveler not found.'})
+        }
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message: 'Server Error occured'});
+    }
+}
