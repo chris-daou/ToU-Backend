b726b85fa97fa7429db91b431ec2586e87f6c63b
vickcodes <vicken.k8@gmail.com>
2023-04-18 11:36:30 +0300

    Added Product model

---
+++ models/Product.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+
+const productSchema = new mongoose.Schema({
+    title:{
+        type: String,
+    },
+    asin:{
+        type: String,
+    },
+    price:{
+        type: String,
+        default: ""
+    },
+    dimensions:{
+        type: String
+    },
+    weight: {
+        type: Number
+    },
+    length: {
+        type: Number
+    },
+    width: {
+        type: Number
+    },
+    height: {
+        type: Number
+    },
+    inStock:{
+        type: Boolean
+    },
+    url:{
+        type: String
+    },
+    quantity_ordered:{
+        type: Number
+    }
+})
+
+
+
+
+
+const Product = mongoose.model('product', productSchema);
+
+module.exports = Product;
