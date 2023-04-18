be3188ba3114413969fe50fc5b5b28ba14e51ad3
chris-daou <117108446+chris-daou@users.noreply.github.com>
2023-04-18 12:32:58 +0300

    added traveler provide pickup

--- controllers/TravelerController.js
+++ controllers/TravelerController.js
@@ -134,3 +134,42 @@ module.exports.cancel_flight = async (req, res) => {
         res.status(400).json({ message: 'Failed to cancel flight' });
     }
 };
+
+module.exports.providePickup_post = async (req, res) => {
+    const travelerId = req.traveler._id;
+    const loc = req.body.location;
+    const traveler = await Traveler.findById(travelerId);
+    for(let i = 0; i < traveler.assigned_orders.length; i++) {
+        const order = await Order.findById(traveler.assigned_orders[i]);
+        order.pickup_location = loc;
+        await order.save();
+    };
+    for(let i = 0; i < traveler.new_orders.length; i++) {
+        const order = await Order.findById(traveler.new_orders[i]);
+            order.waiting_resp = false;
+            order.traveler = "";
+            await order.save();
+            let mailOptions = {
+                from: 'donotreply.tou.lebanon@outlook.com', // your email address
+                to: trav.email, // recipient's email address
+                subject: 'ToU: Order back to pending',
+                text: 'Dear ' + (await User.findById(order.client).name) + ' ' + (await User.findById(order.client).lastname) + ',\n\n' + 'Unfortunately, the traveler we assigned the order to did not accept it. Your order is still pending to be assigned to another traveler.\nFor more information, please contact us.\n\n' + 'Best regards,\n' + 'The ToU Team'
+            };
+            await new Promise((resolve, reject) => {
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        console.log(error);
+                        reject(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                        console.log(link);
+                        resolve();
+                    }
+                });
+            });
+    };
+    traveler.provided_pickup = true;
+    traveler.new_orders = [];
+    traveler.save();
+    res.send('Done');
+};
