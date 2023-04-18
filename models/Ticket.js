6fb2e23fc38d1bbd8f457d686885abe2c6eece92
chris-daou <117108446+chris-daou@users.noreply.github.com>
2023-04-18 11:35:28 +0300

    added ticket model

---
+++ models/Ticket.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const ticketSchema = new mongoose.Schema({
+    pdf_file: {
+        type: String,
+
+    },
+    departure: {
+        type: String,
+        default: ""
+    },
+    return: {
+        type: String,
+        default: ""
+    },
+    departure_flight: {
+        type: String,
+        default: ""
+    },
+    return_flight: {
+        type: String,
+        default: ""
+    },
+    ticket_name:{
+        type: String, 
+        default: ""
+    },
+    traveler: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Traveler'
+    },
+    order: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Order'
+    },
+    valid:{
+        type: Boolean,
+    }
+
+})
+
+const Ticket = mongoose.model('ticket', ticketSchema);
+
+module.exports = Ticket;
