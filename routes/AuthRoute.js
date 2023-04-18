a335f8e426b36b3ae486a10ea56ed0c0d70d5702
vickcodes <vicken.k8@gmail.com>
2023-04-18 16:24:30 +0300

    Added Authentication Routes

---
+++ routes/AuthRoute.js
@@ -0,0 +1,43 @@
+const { Router } = require('express');
+const authController = require('../controllers/authController');
+const rpfpController = require('../controllers/passresetController');
+const tauthController = require('../controllers/travelerauthContoller');
+const trpfpController = require('../controllers/tpassresetController');
+
+const router = Router();
+
+//User
+//login+signup
+router.get("/signup", authController.singup_get);
+router.post("/signup", authController.singup_post);
+router.get("/login", authController.login_get);
+router.post("/login", authController.login_post);
+router.get('/logout', authController.logout_get);
+
+//forgot-password
+router.get("/forgot-password", rpfpController.fp_get);
+router.post("/forgot-password", rpfpController.fp_post);
+router.get("/reset-password/:id/:token", rpfpController.rp_get);
+router.post("/reset-password/:id/:token", rpfpController.rp_post);
+
+//Traveler
+//login+signup
+// router.get("/travelersignup", tauthController.tsingup_get);
+router.post("/travelersignup", tauthController.tsignup_post);
+// router.get("/travelerlogin", tauthController.tlogin_get);
+// router.post("/travelerlogin", tauthController.tlogin_post);
+// router.get('/travelerlogout', tauthController.tlogout_get);
+
+//forgot-password
+// router.get("/travelerforgot-password", trpfpController.tfp_get);
+// router.post("/travelerforgot-password", trpfpController.tfp_post);
+// router.get("/travelerreset-password/:id/:token", trpfpController.trp_get);
+// router.post("/travelerreset-password/:id/:token", trpfpController.trp_post);
+
+//Email Verification
+router.get("/confirm-email/:id/:token", authController.confirmEmail_get);
+
+
+
+
+module.exports = router;
