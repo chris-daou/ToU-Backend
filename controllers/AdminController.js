1bff4b9fef69d2cdb2d7f019e4d6322f2142540d
vickcodes <vicken.k8@gmail.com>
2023-04-18 12:53:29 +0300

    Added Admin API to unrevoke a traveler

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -460,3 +460,23 @@ module.exports.getrevokedTraveler_get = async(req, res) => {
         res.status(500).json({message: 'Server Error occured'});
     }
 }
+
+
+
+module.exports.unrevoke_post = async(req, res) => {
+    const travelerId = req.params.travelerid;
+    const traveler = await Traveler.findById(travelerId);
+    try{
+        if(traveler){
+            traveler.revoked = false;
+            traveler.active = true;
+            traveler.save();
+            res.status(200).json({traveler, message: 'Successfully Unrevoked Access'});
+        }else{
+            res.status(404).json( {message: 'Traveler not found.'})
+        }
+    }catch(err){
+        console.log(err);
+        res.status(500).json({message: 'Server Error occured'});
+    }
+}
