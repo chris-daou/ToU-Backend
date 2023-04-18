d2fa0b2bf6fa5b7c835467c6f1ae6c23c8228397
vickcodes <vicken.k8@gmail.com>
2023-04-18 12:06:54 +0300

    Added Product Controller email

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -7,3 +7,35 @@ const request = require('request-promise')
 const { requireAuth, checkUser } = require('../middleware/authMiddleware');
 const nodemailer = require('nodemailer');
 const { copy } = require('../routes/AdminRoute');
+
+
+
+
+
+let transporter = nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'donotreply.tou.lebanon@outlook.com', // your email address
+      pass: '*31&pCbE' // your email password
+    }
+  });
+  
+  
+  const sendDecisionEmail = (email, name, lastname, title, result) => {
+    let mailOptions = {
+        from: 'donotreply.tou.lebanon@outlook.com', // your email address
+        to: email, // recipient's email address
+        subject: 'ToU: Email Confirmation',
+        text: 'Dear ' + name + ' ' + lastname + ',\n\n' + 'This email has been sent to inform you that your order:\n'+ title + '\nhas been ' + result + '.'
+        };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+          console.log(link);
+        }
+      });
+  }
